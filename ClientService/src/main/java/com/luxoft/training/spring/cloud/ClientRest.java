61f69fa448f2bf93e7a243b32646529484ca3eb5
owlet_2003@mail.ru <user.gmail.com>
2017-10-19 14:04:11 +0300

    Lab 1 Task 2 finished

--- ClientService/src/main/java/com/luxoft/training/spring/cloud/ClientRest.java
+++ ClientService/src/main/java/com/luxoft/training/spring/cloud/ClientRest.java
@@ -1,4 +1,49 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class ClientRest {
+
+    @Autowired
+    private ClientDAO clientDAO;
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @GetMapping("/create")
+    public ClientEntity create(@RequestParam String name) {
+        return clientDAO.create(name);
+    }
+
+    @GetMapping("/update/{id}")
+    public ResponseEntity<Object> update(@PathVariable Integer id, @RequestParam String name) {
+        if (clientDAO.update(id, name)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/delete/{id}")
+    public void delete(@PathVariable Integer id) {
+        clientRepository.delete(id);
+    }
+
+    @GetMapping("/get")
+    public List<ClientEntity> findAll() {
+        return clientRepository.findAll();
+    }
+
+    @GetMapping("/get/{id}")
+    public ClientEntity findOne(@PathVariable Integer id) {
+        return clientRepository.findOne(id);
+    }
 }
--- ClientService/src/main/java/com/luxoft/training/spring/cloud/ClientService.java
+++ ClientService/src/main/java/com/luxoft/training/spring/cloud/ClientService.java
@@ -1,4 +1,14 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
 public class ClientService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientService.class, args);
+    }
 }
