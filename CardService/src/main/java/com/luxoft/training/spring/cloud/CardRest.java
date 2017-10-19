14965dee739bc46ad7c921e588e012b9d473858f
owlet_2003@mail.ru <user.gmail.com>
2017-10-19 14:32:20 +0300

    Lab 1 Task 4 finished

--- CardService/src/main/java/com/luxoft/training/spring/cloud/CardRest.java
+++ CardService/src/main/java/com/luxoft/training/spring/cloud/CardRest.java
@@ -1,4 +1,17 @@
 package com.luxoft.training.spring.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CardRest {
+
+    @Autowired
+    private CardNumberGenerator cardNumberGenerator;
+
+    @GetMapping("/create")
+    public String create() {
+        return cardNumberGenerator.generate();
+    }
 }
